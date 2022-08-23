d88e01217233bc4a1584659e7122e0696fd6e864
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-23 21:50:02 +0300

    Parse jwt token using rsa public key

---
+++ src/main/java/com/odeyalo/netflix/jwt/RsaPublicKeyJwtTokenParser.java
@@ -0,0 +1,40 @@
+package com.odeyalo.netflix.jwt;
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.PublicKey;
+
+public class RsaPublicKeyJwtTokenParser implements JwtTokenParser {
+
+    private final Logger logger = LoggerFactory.getLogger(RsaPublicKeyJwtTokenParser.class);
+
+    @Override
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(RsaTokenHolder.getPublicKey()).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            this.logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            this.logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            this.logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            this.logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            this.logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public Claims getBody(String token) {
+        PublicKey publicKey = RsaTokenHolder.getPublicKey();
+        if (publicKey == null) {
+            throw new IllegalArgumentException("Public key cannot be null!");
+        }
+        return (Claims) Jwts.parser().setSigningKey(publicKey).parse(token).getBody();
+    }
+}
